5d43ee3149c7bbe0c2223ab937ed9cdaf870e3ec
Dhruv Singh Jamwal <142426205+dhruvjamwal@users.noreply.github.com>
2026-02-12 21:07:57 +0530

    Update heap.js

--- js/heap.js
+++ js/heap.js
@@ -1,20 +1,15 @@
-// --- js/heap.js ---
 
 function swap(arr, a, b) {
     let temp = arr[a];
     arr[a] = arr[b];
     arr[b] = temp;
 }
-
 function parent(index) {
     return Math.floor((index - 1) / 2);
 }
-
-// Main function used by app.js
 function makeHeap(arr) {
-    let i;  // Index of next element to be added
-    let k;  // Index of new element as it is being pushed
-
+    let i;  
+    let k;  
     for (i = 1; i < arr.length; ++i) {
         k = i;
         while (k > 0 && arr[k] > arr[parent(k)]) {
